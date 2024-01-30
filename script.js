7670187ce622f8f5de968fe6532c82f442b6f411
obsy AGENCY <amitrathore04984@gmail.com>
2024-01-30 09:54:04 +0530

    Page 2 is cimpleted

--- script.js
+++ script.js
@@ -39,8 +39,62 @@ function loaderpage(){
  Shery.mouseFollower() 
  Shery.makeMagnet("#nav h4") 
 
-  
-
-
-
-  
+ function page2Animation(){
+    var videoC = document.querySelector("#video-container")
+    videoC.addEventListener("mouseenter", function () {
+        gsap.to(".mousefollower", {
+            opacity: 0
+        })
+    })
+    videoC.addEventListener("mouseleave", function () {
+        gsap.to(".mousefollower", {
+            opacity: 1
+        })
+   
+        gsap.to("#play-btn", {
+            left: "70%",
+            top: "-15%"
+        })
+    })
+   
+    var videoImage = document.querySelector("#video-container img")
+    var videoVideo = document.querySelector("#video-container video")
+   
+    var flag = 0
+   
+    videoC.addEventListener("click", function () {
+        if (flag == 0) {
+   
+            gsap.to(videoVideo, {
+                opacity: 1
+            })
+   
+            gsap.to("#play-btn", {
+                scale: 0.8
+            })
+            document.querySelector("#play-btn").innerHTML = '<i class="ri-pause-line"></i>'
+            videoVideo.play()
+            flag = 1
+        } else {
+            gsap.to(videoVideo, {
+                opacity: 0
+            })
+            gsap.to("#play-btn", {
+                scale: 1
+            })
+            document.querySelector("#play-btn").innerHTML = '<i class="ri-play-fill"></i>'
+   
+            videoVideo.pause()
+            flag = 0
+        }
+    })
+   
+    videoC.addEventListener("mousemove", function (dets) {
+        gsap.to("#play-btn", {
+            left: dets.x - 555,
+            top: dets.y - 200
+        })
+    })
+   
+ }
+ page2Animation()
